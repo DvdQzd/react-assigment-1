ee8539b56022d78fadf95ad4f1f7487cba13f272
BloodyLink <david.quezada.m@gmail.com>
2020-03-27 10:35:21 -0300

    Updates

---
+++ precache-manifest.ff0a10a814556dec8bcfae74172fb788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0164a096cd67693dde29b5dcffd9dd",
+    "url": "/react-assigment-1/index.html"
+  },
+  {
+    "revision": "40c081c356cd28fedd64",
+    "url": "/react-assigment-1/static/css/main.cf8ac3d4.chunk.css"
+  },
+  {
+    "revision": "f47f58498ab73b6e5b41",
+    "url": "/react-assigment-1/static/js/2.73ee5223.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-assigment-1/static/js/2.73ee5223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c081c356cd28fedd64",
+    "url": "/react-assigment-1/static/js/main.bfb9ac60.chunk.js"
+  },
+  {
+    "revision": "dc9acccedb9102db4310",
+    "url": "/react-assigment-1/static/js/runtime-main.49e2101e.js"
+  }
+]);
